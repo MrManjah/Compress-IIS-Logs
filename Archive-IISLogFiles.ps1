35986cc433c8f1e8ee17708503964876b79860ea
MrManjah <31627375+MrManjah@users.noreply.github.com>
2024-10-14 00:57:35 +0200

    🍄 Optimisation automnale

---
+++ Archive-IISLogFiles.ps1
@@ -0,0 +1,58 @@
+﻿# Vérification de l'installation de IIS 
+# (Get-WindowsFeature -name Web-Server).InstallState
+
+# Importation de l'assemblage pour la compression
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+
+# Définition de l'emplacement des logs IIS
+$logDir = "C:\inetpub\logs\LogFiles"
+
+# Définition de l'emplacement des archives
+$archiveDir = "C:\inetpub\logs\LogFiles\Archives\"
+
+# Définition de l'emplacement de travail
+$workDir = "C:\inetpub\logs\LogFiles\Temp\"
+
+# Création du répertoire d'archives s'il n'existe pas
+if (!(Test-Path $archiveDir)) {
+    New-Item -Path $archiveDir -ItemType Directory
+    Write-Host "Création du répertoire $($archiveDir)" -ForegroundColor Green
+}
+
+# Définition du délai de rétention (90 jours)
+$logLimit = (Get-Date).AddDays(-90)
+
+# Récupération des fichiers plus anciens que le délai de rétention
+$logFiles = Get-ChildItem -Path $logDir -Filter *.log -Recurse | Where-Object { $_.LastWriteTime -lt $logLimit }
+Write-Host "Nombre de fichiers à archiver : $($logFiles.Count)" -ForegroundColor Green
+
+# Groupement des fichiers par mois et année
+$groups = $logFiles | Group-Object { $_.LastWriteTime.ToString("MM-yyyy") }
+
+foreach ($group in $groups) {
+    # Création du répertoire de travail s'il n'existe pas
+    if (!(Test-Path $workDir)) {
+        New-Item -Path $workDir -ItemType Directory
+        Write-Host "Création du répertoire $($workDir)" -ForegroundColor Green
+    }
+
+    # Génération du nom du fichier zip destination
+    $zipFileName = Join-Path -Path $archiveDir -ChildPath "$($group.Name).zip"
+
+    # Déplacement des fichiers dans le dossier de travail
+    Write-Host "Archivage des fichiers dans $zipFileName" -ForegroundColor Green
+    $group.Group | Move-Item -Destination $workDir -Force
+
+    # Archivage des fichiers dans le fichier destination
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($workDir, $zipFileName, $compressionLevel, $false)
+
+    # Suppression du répertoire de travail et de son contenu
+    Write-Host "Suppression du répertoire $($workDir)" -ForegroundColor Green
+    Remove-Item -Path $workDir -Recurse -Force -Confirm:$false
+}
+
+# Nettoyage : Si vous souhaitez supprimer le dossier de travail même s'il est vide
+if (Test-Path $workDir) {
+    Remove-Item -Path $workDir -Recurse -Force -Confirm:$false
+}
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# Compress-IIS-Logs
+# Archive-IISLogFiles
 
-Script d'archivage de log IIS
+Ce script PowerShell est conçu pour archiver les fichiers journaux IIS (Internet Information Services) en les compressant dans des fichiers ZIP, tout en gérant les fichiers plus anciens que 90 jours.
+
+## Fonctionnalités principales
+
+- **Vérification de l'installation d'IIS :** Le script inclut un contrôle de l'installation d'IIS, bien que ce contrôle soit commenté par défaut.
+- **Gestion des répertoires :** 
+  - Création d'un répertoire d'archives pour stocker les fichiers ZIP générés, s'il n'existe pas déjà.
+  - Création d'un répertoire temporaire pour déplacer les fichiers journaux avant compression.
+- **Filtrage des fichiers :** Le script recherche tous les fichiers journaux (`*.log`) dans le répertoire de logs IIS qui ont été modifiés il y a plus de 90 jours.
+- **Groupement par date :** Les fichiers journaux sont regroupés par mois et année, facilitant l'organisation et la gestion des archives.
+- **Compression :** Les fichiers de chaque groupe sont déplacés vers un répertoire temporaire, puis compressés en un fichier ZIP, avec un niveau de compression optimal.
+- **Nettoyage :** Après l'archivage, le script supprime le répertoire temporaire utilisé pour le stockage des fichiers avant compression.
+
+## Utilisation
+
+Ce script est idéal pour les administrateurs système souhaitant gérer efficacement l'espace de stockage en archivant les anciens fichiers journaux IIS. Il contribue à maintenir un environnement de serveur propre et organisé tout en permettant un accès facile aux fichiers journaux archivés.
